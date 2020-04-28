eb668cbac1b71907014ab3bc9c4463c14a0186e6
Aliceekb <63403854+Aliceekb@users.noreply.github.com>
2020-04-28 16:30:43 +0200

    Add files via upload

---
+++ data/Htelhospitalierpasdelitsdera_3.js
@@ -0,0 +1,9 @@
+var json_Htelhospitalierpasdelitsdera_3 = {
+"type": "FeatureCollection",
+"name": "Htelhospitalierpasdelitsdera_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "nvtablea_1": "AP-HP", "nvtablea_2": "HOPITAL CHARDON LAGACHE", "nvtablea_3": "Centre de gérontologie, médecine gériatrique", "nvtablea_4": "++", "nvtablea_6": "11 RUE CHARDON LAGACHE", "nvtablea_9": "Encadrement", "nvtablea10": "01 44 96 34 19", "nvtablea11": null, "nvtablea12": null, "nvtablea13": null, "nvtablea14": null }, "geometry": { "type": "Point", "coordinates": [ 2.2668309, 48.8449824 ] } },
+{ "type": "Feature", "properties": { "nvtablea_1": "-", "nvtablea_2": "CENTRE NATIONAL D ENTRAINEMENT DE LA FEDERATION FRANCAISE DE TENNIS", "nvtablea_3": "Hotel hospitalier\/ Possibilité de transfert de patients sous oxygénothérapie (faible débit en O2\/ fin de sevrage)", "nvtablea_4": "+++", "nvtablea_6": "4 PLACE DE LA PORTE MOLITOR", "nvtablea_9": null, "nvtablea10": null, "nvtablea11": null, "nvtablea12": "Bernard MONTALVAN", "nvtablea13": "06 86 37 14 16", "nvtablea14": "Mail : bmontalvan@fft.fr" }, "geometry": { "type": "Point", "coordinates": [ 2.257610928040287, 48.846483502707791 ] } }
+]
+}
---
+++ data/MCOSSRaveclitsdera_5.js
@@ -0,0 +1,8 @@
+var json_MCOSSRaveclitsdera_5 = {
+"type": "FeatureCollection",
+"name": "MCOSSRaveclitsdera_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "nvtablea_1": "HEXAGONE", "nvtablea_2": "CLINIQUE BIZET", "nvtablea_3": "Médecine : cardiologie, pneumologie, oncologie", "nvtablea_4": "++", "nvtablea_6": "23 RUE GEORGES BIZET", "nvtablea_9": "Accueil\/ Standard", "nvtablea10": "01 40 69 34 34", "nvtablea11": null, "nvtablea12": "Médecin régulateur Covid", "nvtablea13": "01 40 69 36 10", "nvtablea14": null }, "geometry": { "type": "Point", "coordinates": [ 2.2968021, 48.8676646 ] } }
+]
+}
